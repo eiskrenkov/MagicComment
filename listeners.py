fb891893040d2c60811c3067173c87a5ec0250bd
Egor Iskrenkov <egor@iskrenkov.me>
2022-03-13 23:49:09 +0400

    Allow to run manually

--- listeners.py
+++ listeners.py
@@ -3,4 +3,7 @@ import sublime_plugin
 
 class MagicCommentListener(sublime_plugin.EventListener):
     def on_pre_save(self, view):
-        sublime.active_window().run_command("magic_comment_insert")
+        settings = sublime.load_settings("MagicComment.sublime-settings")
+
+        if settings.get("run_on_save"):
+            sublime.active_window().run_command("magic_comment_insert")
